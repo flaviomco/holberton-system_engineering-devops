d1c109fb5bd8a3869ba70619bcda1d99793883af
flaviomco <2109@holbertonschool.com>
2021-04-03 18:06:41 +0000

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#  Client configuration file (w/ Puppet)
+
+file_line { 'Turn off passwd auth':
+    ensure => 'present',
+    line   => '    PasswordAuthentication no',
+    path   => '/etc/ssh/ssh_config',
+}
+file_line { 'Declare identity file':
+    ensure => 'present',
+    line   => '    IdentityFile ~/.ssh/holberton',
+    path   => '/etc/ssh/ssh_config',
+}
