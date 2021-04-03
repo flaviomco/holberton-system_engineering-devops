3587724c27ccd1985c50089c843324ee246d76e0
flaviomco <2109@holbertonschool.com>
2021-04-03 18:12:25 +0000

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Turns off password authentication and uses holberton key
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
+file_line { 'private key':
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  ensure => 'present',
 }
 
-file_line { 'Declare identity file':
-  ensure => 'present',
+file_line { 'refuse to authenticate':
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
+  line   => 'PasswordAuthentication no',
+  ensure => 'present',
 }
