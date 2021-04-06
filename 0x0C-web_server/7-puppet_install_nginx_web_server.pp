bdafd5add6699d8d9cb2ee98aa892a991662437d
flaviomco <2109@holbertonschool.com>
2021-04-06 13:06:12 +0000

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,21 @@
-# update +install ngnix 
+# Install Nginx web server (w/ Puppet)
 
-exec {'up':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y update',
+package { 'nginx':
+  ensure => installed,
 }
 
-# install ngnix 
-exec {'ngin':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y install nginx',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
 
-exec {'conf':
-  provider => shell,
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/google.com\/ permanent;/" /etc/nginx/sites-available/default',
+file_line { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/ permanent;',
 }
 
-exec { 'upindex':
-  provider => shell,
-  command => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
-}
-
-
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx start',
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Holberton School',
 }
